d23f0d6360bde47e2a2d33e94b63bd63a0fb04fc
Alexander Kuvaev <alexander@kuvaev.me>
2019-09-27 18:16:10 +0300

    Добавили кнопки

--- main.py
+++ main.py
@@ -54,15 +54,15 @@ def print_err(s):
 
 @app.route('/js/<path:path>')
 def send_js(path):
-    return send_from_directory('include/js', path)
+    return send_from_directory('js', path)
 
 @app.route('/css/<path:path>')
 def send_css(path):
-    return send_from_directory('include/css', path)
+    return send_from_directory('css', path)
 
 @app.route('/fonts/<path:path>')
 def send_fonts(path):
-    return send_from_directory('include/fonts', path)
+    return send_from_directory('fonts', path)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
